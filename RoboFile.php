d1b98674c377d6ef97fd86bc7a2e3ff2eb63f379
EasyChen <easychen@qq.com>
2024-02-08 17:40:30 +0800

    add docs folder

---
+++ RoboFile.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This is project's console commands configuration for Robo task runner.
+ *
+ * @see https://robo.li/
+ */
+class RoboFile extends \Robo\Tasks
+{
+    // define public methods as commands
+    public function build()
+    {
+        $this->taskExec('cd web && npm install && npm run build')
+            ->run();
+
+        // move web/dist to ./docs
+        $this->taskExec('mv web/dist docs')->run();
+
+    }
+}
